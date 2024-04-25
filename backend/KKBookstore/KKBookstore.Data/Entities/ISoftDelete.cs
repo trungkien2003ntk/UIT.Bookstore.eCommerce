5be8430ecd3c1630d0be421e45b5570b738a0b76
TrungKien <trungkien2003ntk@gmail.com>
2024-04-25 20:15:00 +0700

    Initial Project

---
+++ backend/KKBookstore/KKBookstore.Data/Entities/ISoftDelete.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace KKBookstore.Data.Entities
+{
+    public interface ISoftDelete
+    {
+        [DefaultValue(false)]
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset? DeletedAt { get; set; }
+        
+        public void Undo()
+        {
+            IsDeleted = false;
+            DeletedAt = null;
+        }
+    }
+}
---
+++ backend/KKBookstore/KKBookstore.Data/Entities/ShippingAddress.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KKBookstore.Data.Entities
+{
+
+    [Table("ShippingAddress")]
+    public class ShippingAddress : BaseEntity, ISoftDelete
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ShippingAddressId { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Province { get; set; }
+
+        public string District { get; set; }
+
+        public string Ward { get; set; }
+
+        public string Street { get; set; }
+
+        public string AddressType { get; set; }
+
+        public string? Note { get; set; }
+
+        public bool IsDefault { get; set; }
+
+        public User? User { get; set; }
+        
+        public bool IsDeleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public DateTimeOffset? DeletedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}

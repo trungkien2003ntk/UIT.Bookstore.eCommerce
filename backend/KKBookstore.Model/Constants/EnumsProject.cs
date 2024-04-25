a976da9338bfc9fe7fcf02eba7570cf529c4296c
TrungKien <trungkien2003ntk@gmail.com>
2024-04-25 20:22:23 +0700

    Initial Push

---
+++ backend/KKBookstore.Model/Constants/EnumsProject.cs
@@ -0,0 +1,41 @@
+﻿namespace KKBookstore.Model.Constants
+{
+    public enum UserStatus
+    {
+        Active,
+        Inactive,
+        Comfirm,
+        UnComfirm
+    }
+
+    public enum OrderStatus
+    {
+        Pending,
+        PaymentProcessing,
+        PaymentConfirmed,
+        AwaitingFulfillment,
+        Packed,
+        Shipped,
+        OutForDelivery,
+        Delivered,
+        Cancelled
+    }
+
+    public enum StockStatus
+    {
+        InStock,
+        LowStock,
+        OutOfStock
+    }
+
+    public enum TypeLogin
+    {
+        Google,
+        Facebook,
+        Email
+    }
+
+    
+
+
+}
